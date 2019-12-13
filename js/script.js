1ab3677523cd848eb36468721a72c5678d19cc38
zek0hh <blanchetantoine83@gmail.com>
2019-12-13 02:36:12 +0100

    last commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //Animations
+/*
 ScrollReveal().reveal('.headline', {
     easing: 'ease-in',
     duration: 500,
@@ -27,7 +28,7 @@ ScrollReveal().reveal('#scrollPicto', {
     easing: 'ease-in',
     duration: 500,
     reset: true
-});
+});*/
 
 ScrollReveal().reveal('#cortisol', {
     easing: 'ease-in',
@@ -68,7 +69,7 @@ ScrollReveal().reveal('.media-is-right', {
     reset: true
 });
 
-
+/*
 ScrollReveal().reveal('.campain-column', {
     easing: 'ease-in',
     distance: '50px',
@@ -96,7 +97,7 @@ ScrollReveal().reveal('.orange-center', {
     delay: 500,
     reset: true
 });
-
+*/
 ScrollReveal().reveal('.is-one-fifths', {
     easing: 'ease-in',
     origin: 'top',
@@ -106,7 +107,7 @@ ScrollReveal().reveal('.is-one-fifths', {
     delay: 500,
     reset: true
 });
-
+/*
 ScrollReveal().reveal('#inspiration h2', {
     easing: 'ease-in',
     origin: 'top',
@@ -116,14 +117,14 @@ ScrollReveal().reveal('#inspiration h2', {
     delay: 500,
     reset: true
 });
-
+*/
 ScrollReveal().reveal('.is-1', {
     easing: 'ease-in',
     interval: 200,
     duration: 500,
     reset: true
 });
-
+/*
 ScrollReveal().reveal('#inspiration div article div', {
     easing: 'ease-in',
     interval: 200,
@@ -178,44 +179,53 @@ ScrollReveal().reveal('section.association p', {
     easing: 'ease-in',
     duration: 500,
     reset: true
-});
+});*/
 //END OF ANIMATION
 
 
 
 document.getElementById('photo').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="https://www.offi.fr/expositions-musees/photographie.html" class="column" target="_blank">Exposition de photos</a><a href="https://www.kazoart.com/fr/45-photographie" class="column" target="_blank">Achat de photos d\'art</a><a href="https://www.youtube.com/watch?v=qAen30AFzNE" class="column" target="_blank">Vidéo sur la photo</a>';
 };
 document.getElementById('peinture').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="https://www.offi.fr/expositions-musees/beaux-arts.html" class="column" target="_blank">Exposition de peintures</a><a href="https://www.artsper.com/fr/oeuvres-d-art-contemporain/peinture" class="column" target="_blank">Achat de peintures</a><a href="https://www.youtube.com/watch?v=bXQiVJydvDM" class="column" target="_blank">Vidéo sur la peinture</a>';
 };
 document.getElementById('cinema').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="http://www.allocine.fr/" class="column" target="_blank">Séances de cinéma</a><a href="https://www.fnac.com/video.asp?awc=12665_1575907359_119da06b6dfee0e43d7193f357a1b078&ectrans=1&Origin=Awin101248" class="column" target="_blank">Achat de dvds</a><a href="https://www.kazoart.com/blog/le-cinema-dans-lhistoire-de-lart/" class="column" target="_blank">Article sur le cinéma</a>';
 };
 document.getElementById('danse').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="https://www.theatresparisiensassocies.com/spectacle-danse-choregraphique-paris" class="column" target="_blank">Spectacle de danse</a><a href="https://www.fnacspectacles.com/place-spectacle/Danse/Danse-a-Paris-p3122410154794565240.htm" class="column" target="_blank">Billeterie de spectacles de danse</a><a href="https://www.ubisoft.com/fr-fr/game/just-dance-2019" class="column" target="_blank">Jeu de danse</a>';
 };
 document.getElementById('theatre').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="https://www.offi.fr/theatre" class="column" target="_blank">Théâtre sur Paris</a><a href="https://www.theatresparisiensassocies.com/pieces-theatre-paris/" class="column" target="_blank">Spectacles au théâtre</a><a href="https://www.fnac.com/n6997/Humour-Theatre-DVD-Blu-Ray/Theatre?awc=12665_1576167774_ea7981e4481604e184b3cb6e379d166b&ectrans=1&Origin=Awin101248" class="column" target="_blank">DVDs issus de théâtres</a>';
 };
 document.getElementById('scene').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="https://www.fnacspectacles.com/place-spectacle/humour/humour-7110577968918422048-lt.htm" class="column" target="_blank">Spéctacle d\'humour</a><a href="https://www.billetreduc.com/plateau-d-humoristes/r/91/liste.html" class="column" target="_blank">Spectacles humoristiques</a><a href="https://www.fnac.com/n1865/Humour-Theatre-DVD-Blu-Ray/Humour-et-Spectacles" class="column" target="_blank">DVDs humoristiques</a>';
 };
 document.getElementById('musique').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="https://philharmoniedeparis.fr/fr/musee-de-la-musique/expositions" class="column" target="_blank">Exposition sur la musique</a><a href="https://www.youtube.com/playlist?list=PLKxspWK8-EBAG02ZcuFVMnDzyEbmPXyvI" class="column" target="_blank">Musique relaxante</a><a href="https://www.spotify.com/fr/" class="column" target="_blank">Plateforme de musique en ligne</a>';
 };
 document.getElementById('architecture').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="https://www.offi.fr/expositions-musees/architecture-design.html" class="column" target="_blank">Exposition architecture</a><a href="https://www.youtube.com/watch?v=jMOzC2U8zfw" class="column" target="_blank">Vidéo sur l\'architecture</a><a href="https://www.parisinfo.com/visiter-a-paris/monuments/monuments-incontournables" class="column" target="_blank">Visiter des monuments d\'architectures célèbres</a>';
 };
 document.getElementById('litterature').onclick = function() {
+    document.getElementById('cadre').style.display = 'block';
     document.getElementById('boite').innerHTML = '';
     document.getElementById('boite').innerHTML = '<a href="http://www.parismusees.paris.fr/fr/activites/conferences-litteraires" class="column" target="_blank">Conférence littéraire</a><a href="https://www.52we.com/fr/guides/week-end-au-coeur-du-paris-litteraire.html" class="column" target="_blank">Evenements littéraires</a><a href="https://www.pagesjaunes.fr/annuaire/paris-75/livres-anciens" class="column" target="_blank">Adresses de librairies anciennes</a>';
 };
